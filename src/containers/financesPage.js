79937b83ab2d9f08fdd85a3c680ea0f28d809fa2
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-09-28 19:52:46 -0500

    adds actions for finances and a container page

---
+++ src/containers/financesPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from '../actions/fuelSavingsActions';
+import FuelSavingsForm from '../components/FuelSavingsForm';
+
+export const FuelSavingsPage = (props) => {
+  return (
+    <div>Hi</div>
+  );
+};
+function mapStateToProps(state) {
+  return {
+    fuelSavings: state.fuelSavings
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FuelSavingsPage);
